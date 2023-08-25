f7fe7873e585da9c452d5360cdf72eb3b2dd3bb7
Ahmed Samy <115529140+AhmedSamy9999@users.noreply.github.com>
2023-08-25 08:52:22 +0300

    edit globals css

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,9 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={bodyFont.className}>
-        <Navbar />
-        {children}
-        <Footer />
+        <div className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
