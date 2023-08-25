22c20365a057d0025f6f9ae7e264804afeb2515e
Ahmed Samy <115529140+AhmedSamy9999@users.noreply.github.com>
2023-08-26 02:34:16 +0300

    finish footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,23 @@
+import Image from "next/image";
 import styles from "./Footer.module.css";
+import { socialMedia } from "./data";
 
 export default function Footer() {
   return (
     <div className={styles.container}>
       <div>© 2023 Hexashop. All rights reserved</div>
-      <div></div>
+      <div className={styles.social}>
+        {socialMedia.map((media) => (
+          <Image
+            key={media.id}
+            src={`/images/icons/${media.media}.png`}
+            width={20}
+            height={20}
+            className={styles.icon}
+            alt={`Hexashop ${media.media} link`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
